53db7609ae98f8d8269e12394f38b334f9f6ce94
guntas-13 <guntassingh.saran@iitgn.ac.in>
2024-03-10 15:17:12 +0530

    initial changes

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.write("""
+         # First Streamlit App
+         ## This is a simple example of using Streamlit
+         ### $$\Gamma(x) = \int_0^{\infty} e^{-t} t^{x - 1} dt$$
+         """)
+
+st.button('Hit me')
+st.checkbox('Check me out')
+st.radio('Pick one:', ['nose','ear'])
+st.selectbox('Select', [1,2,3])
+st.multiselect('Multiselect', [1,2,3])
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+st.session_state.name
+
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
