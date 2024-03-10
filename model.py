b3c37486e2312a76a7efedac8b81692659503310
guntas-13 <guntassingh.saran@iitgn.ac.in>
2024-03-11 01:59:08 +0530

    Trained and Saved the model

---
+++ model.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+import pandas as pd
+import matplotlib.pyplot as plt
+from latex import latexify, format_axes
+from pprint import pprint
+from sklearn.manifold import TSNE
+import torch._dynamo
+import warnings
+warnings.filterwarnings("ignore")
+torch._dynamo.config.suppress_errors = True
+
+if (torch.cuda.is_available()):
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+print(f"Supported Device: {device}\n\n")
+
+
+fileTolstoy = open("tolstoy.txt", "r")
+tolstoy = fileTolstoy.read()
+
+new_tolstoy = ""
+for char in tolstoy:
+    if char in ['à', 'ä', 'é', 'ê']:
+        continue
+    new_tolstoy += char
+
+characters = sorted(list(set(new_tolstoy)))
+print(f"Total Characters in the Corpus: {len(new_tolstoy)}\n")
+print(f"Number of Characters in the Corpus: {len(characters)}\n")
+pprint(f"The characters are:\n{characters}\n\n")
+
+
+stoi = {s : i + 1 for i, s in enumerate(characters)}
+stoi["_"] = 0
+itos = {i : s for s, i in stoi.items()}
+pprint(f"Dictionary Mapping of indices to characters:\n{itos}\n\n")
+
+
+block_size = 10
+start = 0
+X, Y = [], []
+context = [0] * block_size
+for idx in range(len(new_tolstoy)):
+  ix = stoi[new_tolstoy[idx]]
+  X.append(context)
+  Y.append(ix)
+  context = context[1:] + [ix]
+
+X = torch.tensor(X).to(device)
+Y = torch.tensor(Y).to(device)
+
+print(f"Training Samples: {X.shape}\nLabels:{Y.shape}\n\n")
+
+
+emb_dim = 15
+emb = torch.nn.Embedding(len(stoi), emb_dim)
+print(f"Embeddings Shape: {emb.weight.shape}\n\n")
+
+
+latexify(columns = 2, fig_width = 10)
+
+def plot_emb(emb, itos, ax=None):
+    if emb.weight.shape[1] != 2:
+      tsne = TSNE(n_components = 2)
+      emb_new = tsne.fit_transform(emb.weight.detach().cpu().numpy())
+    if ax is None:
+        fig, ax = plt.subplots()
+    for i in range(len(itos)):
+        if emb.weight.shape[1] == 2:
+          x, y = emb.weight[i].detach().cpu().numpy()
+          ax.scatter(x, y, color='k')
+          ax.text(x + 0.05, y + 0.05, itos[i])
+          ax.set_title("2D Embeddings Before Training")
+        else:
+          x, y = emb_new[i]
+          ax.scatter(x, y, color = 'k')
+          ax.text(x + 0.05, y + 0.05, itos[i])
+          ax.set_title("2D Embeddings using t-SNE After Training")
+    return ax
+
+plot_emb(emb, itos)
+
+
+
+class NextChar(nn.Module):
+  def __init__(self, block_size, vocab_size, emb_dim, hidden_size):
+    super().__init__()
+    self.emb = nn.Embedding(vocab_size, emb_dim)
+    self.lin1 = nn.Linear(block_size * emb_dim, hidden_size)
+    self.lin2 = nn.Linear(hidden_size, vocab_size)
+
+  def forward(self, x):
+    x = self.emb(x)
+    x = x.view(x.shape[0], -1)
+    x = torch.tanh(self.lin1(x))
+    x = self.lin2(x)
+    return x
+
+
+model = NextChar(block_size, len(stoi), emb_dim, 30).to(device)
+model = torch.compile(model)
+print("Model Architecture:\n")
+for param_name, param in model.named_parameters():
+    print(param_name, param.shape)
+
+
+g = torch.Generator()
+g.manual_seed(42)
+def generate_text(model, itos, stoi, block_size, max_len, start_str = None):
+
+    context = [0] * block_size
+    if start_str:
+        for s in start_str:
+            context = context[1:] + [stoi[s]]
+    text = start_str if start_str else ""
+    for i in range(max_len):
+        x = torch.tensor(context).view(1, -1).to(device)
+        y_pred = model(x)
+        ix = torch.distributions.categorical.Categorical(logits = y_pred).sample().item()
+        ch = itos[ix]
+        text += ch
+        context = context[1:] + [ix]
+    return text
+
+
+old_text = generate_text(model, itos, stoi, block_size, 1000, "This is ")
+print("\n==================\n")
+print("Generated Text from Untrained Model: \n")
+print(old_text)
+
+device = torch.device("cpu")
+model.load_state_dict(torch.load("model.pth", map_location = device))
+
+latexify(columns = 2, fig_width = 8)
+plot_emb(model.emb, itos)
+
+print("\n==================\n")
+txt = input("Enter the starting string:\n")
+
+my_str = generate_text(model, itos, stoi, block_size, 1000, "It was the year 2024, we had an ML assignment ")
+print("\n==================\n")
+print("Generated Text from Trained Model: \n")
+print(my_str)
